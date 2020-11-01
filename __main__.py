7dab7adc238c57c01fede7954580ecdf1bec847a
j769chen <j769chen@uwaterloo.ca>
2020-11-01 18:13:11 -0500

    Added functions to get data for a tier, and to get data for all tiers. Added error handling for too many API requests and for match number input.

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ def main(summonerName, numGames, champion, lane, role=None):
     else:
         userStats, tier, division = getData.getUsersStatsToReview(summonerName, lane, numGames, role, champion)
 
-    if lane == LANES['Middle(Match History)']: # Change this back so that it is 'MIDDLE' for match analysis. See roleReference line 26 for reasoning
+    if lane == LANES['Middle(Match History)']:  # Change this back so that it is 'MIDDLE' for match analysis. See roleReference line 26 for reasoning
         lane = LANES['Middle']
 
     if lane == LANES['Bottom']:
@@ -29,12 +29,19 @@ def main(summonerName, numGames, champion, lane, role=None):
 
 
 if __name__ == "__main__":
-    numGames = ""
+    numGames = 0
     print("Welcome to int.gg, an app to see if you've been running it down!")
 
-    summonerName = input("Please enter your summoner name: ")
+    summonerName = input("Please enter your summoner name (case sensitive): ")
 
-    numGames = int(input("Please enter the number of games you would like reviewed (max 10): "))
+    while numGames <= 0 or numGames > 10:
+        try:
+            numGames = int(input("Please enter the number of games you would like reviewed (1-10): "))
+            if numGames <= 0 or numGames > 10:
+                print("That is not a valid number, please enter an integer between 1 and 10.")
+        except ValueError:
+            print("That input is not an integer!")
+
     champion = input("If you would like your matches on a specific champion to be reviewed, please enter that "
                      "champion's name here. If not, click enter: ")
 
--- getData.py
+++ getData.py
@@ -2,43 +2,69 @@ import requests
 import json
 import os
 import math
+import time
 from roleReference import TIERS, DIVISIONS, LANES, BOT_ROLES
 
 """Module with all data fetching/dumping functions"""
 
-API_KEY= "RGAPI-081f2930-4317-4296-844c-23fc0229b3c2"
+API_KEY= "RGAPI-fae4124d-947c-424e-a567-cdbea9687413"
 REGION = 'na1'
-QUEUE = 'RANKED_SOLO_5x5' # Only interested in ranked solo queue data
+QUEUE = 'RANKED_SOLO_5x5'  # Only interested in ranked solo queue data
+
+numrequests = 0
 
 
-def requestSummonerData(summonerName): # Get a summoner's profile based on their username
+def tooManyRequestsError(jsonResponse):  # Function to inform user if they cap out on API requests
+    if jsonResponse == {'status': {'message': 'Gateway timeout', 'status_code': 504}}:
+        print('ERROR: You have exceeded the maximum number of requests to the Riot API')
+        return - 1
+    return 0
+
+
+def requestSummonerData(summonerName):  # Get a summoner's profile based on their username
     URL = "https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}".format(REGION, summonerName, API_KEY)
     response = requests.get(URL)
 
-    return response.json()
+    global numrequests
+    numrequests += 1
+
+    jsonResponse = response.json()
+    tooManyRequestsError(jsonResponse)
+    return jsonResponse
 
 
-def requestRankedData(summonerDict): # Get a summoner's ranked stats from their ID
+def requestRankedData(summonerDict):  # Get a summoner's ranked stats from their ID
     summonerID = summonerDict['id']
     URL = "https://{}.api.riotgames.com/lol/league/v4/entries/by-summoner/{}?api_key={}".format(REGION, summonerID, API_KEY)
     response = requests.get(URL)
 
-    return response.json()
+    global numrequests
+    numrequests += 1
+
+    jsonResponse = response.json()
+    tooManyRequestsError(jsonResponse)
+    return jsonResponse
 
 
-def getRankedTier(tier, division=None): # Get random players from a specific division
+def requestRankedTier(tier, division=None):  # Get random players from a specific division
     baseURL = "https://{}.api.riotgames.com/lol/league/v4".format(REGION)
     if tier == TIERS['Challenger'] or tier == TIERS['Grandmaster'] or tier == TIERS['Master']:
         URL = baseURL + "/{}leagues/by-queue/{}?api_key={}".format(tier.lower(), QUEUE, API_KEY)
     else:
         URL = baseURL + "/entries/{}/{}/{}?api_key={}".format(QUEUE, tier, division, API_KEY)
 
+    print(URL)
     response = requests.get(URL)
 
-    return response.json()
+    global numrequests
+    numrequests += 1
+
+    jsonResponse = response.json()
+    tooManyRequestsError(jsonResponse)
+    return jsonResponse
 
 
-def getMatchHistory(accountID, champion=None): # Get a user's match history with option of filtering by champion
+def requestMatchHistory(accountID, champion=None):  # Get a user's match history with option of filtering by champion
     baseURL = "https://{}.api.riotgames.com/lol/match/v4/matchlists/by-account/{}?queue=420&queue=430&queue=440".format(REGION, accountID)
 
     if champion:
@@ -47,18 +73,27 @@ def getMatchHistory(accountID, champion=None): # Get a user's match history with
         URL = baseURL + "&api_key={}".format(API_KEY)
     response = requests.get(URL)
 
-    return response.json()['matches']
+    global numrequests
+    numrequests += 1
+    jsonResponse = response.json()
+    tooManyRequestsError(jsonResponse)
+    return jsonResponse['matches']
 
 
-def getMatchById(matchId): # Fetch one match's data from its match ID
+def requestMatchById(matchId):  # Fetch one match's data from its match ID
     URL = "https://{}.api.riotgames.com/lol/match/v4/matches/{}?api_key={}".format(REGION, matchId,
                                                                                                  API_KEY)
     response = requests.get(URL)
 
-    return response.json()
+    global numrequests
+    numrequests += 1
+
+    jsonResponse = response.json()
+    tooManyRequestsError(jsonResponse)
+    return jsonResponse
 
 
-def preprocessStats(player): # Function to get other calculated stats before analysis
+def preprocessStats(player):  # Function to get other calculated stats before analysis
     if player['stats']['deaths'] != 0:
         player['stats']['KDA'] = round((player['stats']['kills'] + player['stats']['assists'])/player['stats']['deaths'], 2)
     else:
@@ -69,8 +104,8 @@ def preprocessStats(player): # Function to get other calculated stats before ana
     player['stats']['earlyGameXp'] = player['timeline']['xpPerMinDeltas']['0-10']
 
 
-def getPlayerMatchStats(summonerName, matchId): # Get important stats from one match for a player
-    match = getMatchById(matchId)
+def getPlayerMatchStats(summonerName, matchId):  # Get important stats from one match for a player
+    match = requestMatchById(matchId)
     for participants in match['participantIdentities']:
         if participants['player']['summonerName'] == summonerName:
             playerId = participants['participantId']
@@ -84,7 +119,7 @@ def getPlayerMatchStats(summonerName, matchId): # Get important stats from one m
     return playerStats
 
 
-def filterMatchList(matchList, lane, role, numGames): # Filters match history by either lane or role if lane is "BOTTOM"
+def filterMatchList(matchList, lane, role, numGames):  # Filters match history by either lane or role if lane is "BOTTOM"
     filteredMatchList = []
     if lane == LANES['Bottom']:
         filterParam = role
@@ -94,7 +129,7 @@ def filterMatchList(matchList, lane, role, numGames): # Filters match history by
         query = 'lane'
 
     for matches in matchList:
-        if filterParam == BOT_ROLES['Support']: # Error handling for other lanes having DUO_SUPPORT role tag
+        if filterParam == BOT_ROLES['Support']:  # Error handling for other lanes having DUO_SUPPORT role tag
             if matches['lane'] == lane and matches[query] == filterParam:
                 filteredMatchList.append(matches)
         else:
@@ -111,7 +146,7 @@ def filterMatchList(matchList, lane, role, numGames): # Filters match history by
     return filteredMatchList
 
 
-def getUsersStatsToReview(summonerName, lane, numGames, role=None, champion=None): # Gets users stats for last numGames games from their summoner name, filtering by role and optionally filtering by champions
+def getUsersStatsToReview(summonerName, lane, numGames, role=None, champion=None):  # Gets users stats for last numGames games from their summoner name, filtering by role and optionally filtering by champions
     summonerInfo = requestSummonerData(summonerName)
     rankedInfo = requestRankedData(summonerInfo)
 
@@ -119,7 +154,7 @@ def getUsersStatsToReview(summonerName, lane, numGames, role=None, champion=None
         if leagues['queueType'] == QUEUE:
             tier, division = leagues['tier'], leagues['rank']
 
-    matchHistory = getMatchHistory(summonerInfo['accountId'], champion)
+    matchHistory = requestMatchHistory(summonerInfo['accountId'], champion)
     userStats = []
 
     filteredMatchList = filterMatchList(matchHistory, lane, role, numGames)
@@ -146,7 +181,7 @@ def getFilepath(tier, division, lane, role='', average=False):
     return baseDirectory + jsonName
 
 
-def generateFiles(): # Reset/Regenerate comparison data directories and create new files
+def generateFiles():  # Reset/Regenerate comparison data directories and create new files
     for keys in TIERS:
         if keys == 'Master' or keys == 'Grandmaster' or keys == 'Challenger':
             divisions = {1: 'I'}
@@ -157,7 +192,7 @@ def generateFiles(): # Reset/Regenerate comparison data directories and create n
                 if lanes != 'Middle(Match History)': # Ensures only one mid files is created
                     if lanes == 'Bottom':
                         for roles in BOT_ROLES:
-                            try: # Try except so that if filepath doesn't exist, generate directories before generating file
+                            try:  # Try except so that if filepath doesn't exist, generate directories before generating file
                                 with open(getFilepath(TIERS[keys], DIVISIONS[division], LANES[lanes],
                                                       BOT_ROLES[roles]), 'w') as f:
                                     f.write('')
@@ -176,9 +211,8 @@ def generateFiles(): # Reset/Regenerate comparison data directories and create n
                                 f.write('')
 
 
-def recordMatchStats(tier, division, matchId): # Dump stats from a match to the corresponding division/role JSON file
-    match = getMatchById(matchId)
-    print(match)
+def recordMatchStats(tier, division, matchId):  # Dump stats from a match to the corresponding division/role JSON file
+    match = requestMatchById(matchId)
     for players in match['participants']:
         players['stats']['gameDuration'] = round(match['gameDuration']/60, 2)
         print(players)
@@ -193,23 +227,53 @@ def recordMatchStats(tier, division, matchId): # Dump stats from a match to the 
                 outfile.write('\n')
 
 
-def getDataForDivision(tier, division='I'): # Get ~10 random matches from players that are in a specific tier and division, excluding those with deprecated roles
-    rankedTier = getRankedTier(tier, division)
+def getDataForDivision(tier, division='I'):  # Get ~10 random matches from players that are in a specific tier and division, excluding those with deprecated roles
+    rankedTier = requestRankedTier(tier, division)
     matchHistories = []
-
+    print(rankedTier)
     for i in range(10): # Get match histories from 10 random players in the current tier
         if tier == TIERS['Master'] or tier == TIERS['Grandmaster'] or tier == TIERS['Challenger']:
             summonerData = requestSummonerData(rankedTier['entries'][i]['summonerName'])
         else:
             summonerData = requestSummonerData(rankedTier[i]['summonerName'])
 
-        matchHistories.append(getMatchHistory(summonerData['accountId']))
+        matchHistories.append(requestMatchHistory(summonerData['accountId']))
 
-    for histories in matchHistories: # Get one match from each players match history and dump to file
+    for histories in matchHistories:  # Get one match from each players match history and dump to file
         recordMatchStats(tier, division, histories[0]['gameId'])
 
+
+def getDataForTier(tier):
+    if tier == TIERS['Master'] or tier == TIERS['Grandmaster'] or tier == TIERS['Challenger']:
+        print("Getting data for {}".format(tier))
+        getDataForDivision(tier)
+    else:
+        for i in range(1, 4):
+            print("Getting data for {} {}".format(tier, DIVISIONS[i]))
+            getDataForDivision(tier, DIVISIONS[i])
+
+        print('Waiting 2 min for rate cap to reset...')
+        time.sleep(120)  # Need to do this to avoid rate cap for API requests, which is 100/2min
+
+        print("Getting data for {} {}".format(tier, DIVISIONS[4]))
+        getDataForDivision(tier, DIVISIONS[4])
+
+
+def getDataForAllTiers():  # Fetches data for all tiers, due to rate limits on API requests, takes relatively long amount of time due to needing to break for two minutes every ~100 requests
+    for tier in TIERS:
+        getDataForTier(TIERS[tier])
+        if tier != TIERS['Master'] or tier != TIERS['Grandmaster'] or tier != TIERS['Challenger']:  # Since only 1 division for top 3 tiers, do not need to wait as we can run all three without hitting the cap
+            time.sleep(120)
+
+
+# start_time = time.time()
+# getDataForAllTiers()
+# print(time.time() - start_time())
+
+
+# getDataForTier('PLATINUM')
 # generateFiles()
-# getDataForDivision('PLATINUM', 'IV')
+
 # def getDataForAllDivisions():
 # getDataForDivision('PLATINUM', 'IV')
 # testMatch = getMatchById(3630630530)
